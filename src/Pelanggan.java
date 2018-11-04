7cdce1a226e8d82075c6377d48a4ac37cbc2bea0
Karina Widya <karinawidya9999@gmail.com>
2018-11-04 14:19:48 +0700

    Menambahkan konstruktor berparameter

--- src/Pelanggan.java
+++ src/Pelanggan.java
@@ -11,6 +11,11 @@
 public class Pelanggan {
     private String id;
     private String nama;
+
+    public Pelanggan(String id, String nama) {
+        this.id = id;
+        this.nama = nama;
+    }
     
     
     
