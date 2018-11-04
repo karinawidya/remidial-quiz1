d3cb5bdcdb5ad6bfe052872a0e7c3ca9c47d3a0b
Karina Widya <karinawidya9999@gmail.com>
2018-11-04 14:20:52 +0700

    Menambahkan method getter dan setter

--- src/Pelanggan.java
+++ src/Pelanggan.java
@@ -16,6 +16,22 @@ public class Pelanggan {
         this.id = id;
         this.nama = nama;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
     
     
     
